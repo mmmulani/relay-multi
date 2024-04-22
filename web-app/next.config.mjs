b2373e1389ed9711c2f74e1f0c7bfc13235e1d99
Mehdi Mulani <mars.martian@gmail.com>
2024-04-22 11:22:42 -0700

    Add basic relay support to config

--- web-app/next.config.mjs
+++ web-app/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    relay: {
+      src: './',
+      artifactDirectory: './__generated__',
+      language: 'typescript',
+    },
+  },
+};
 
 export default nextConfig;
